6acbc854e395f349d1ea7a23a7466146335a5c04
Jesse Calton <jessecalton@gmail.com>
2019-07-15 21:03:12 -0700

    adds year, title, metacritic score

--- scraper.rb
+++ scraper.rb
@@ -2,7 +2,15 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-def movie_page(title)
+	# attr_accessor :runtime, :title
+
+def set_movie_data(movie_title)
+	open_movie_page(movie_title)
+	runtime
+	title
+end
+
+def open_movie_page(title)
 	base_url = "https://www.imdb.com/find?ref_=nv_sr_fn&q="
 	title = title.gsub(/\s/, '+')
     search_data = Nokogiri::HTML(open("#{base_url}#{title}&s=tt"))
@@ -16,9 +24,20 @@ def runtime
 
 	minutes = time.gsub(/\d*[h]/) {|s| s.delete('h').to_i * 60}
 	minutes.split(' ').each {|mins| runtime += mins.to_i}
-	@runtime
+	@runtime = runtime
 end
 
+def title
+	@title = @movie.css('title').text.split('(')[0].strip
+end
+
+def year
+	@year = @movie.css('span#titleYear').text.gsub(/[()]/, '')
+end
+
+def metacritic_score
+	@metacritic_score = @movie.css('div.metacriticScore').text.strip.to_i
+end
 
 # https://www.imdb.com/find?ref_=nv_sr_fn&q=evil+dead&s=tt
 # https://www.imdb.com/title/tt0083907/
