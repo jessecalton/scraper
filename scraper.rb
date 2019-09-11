02e1e9ecca0bb81565f165703b34afc1370ab310
Jesse Calton <jessecalton@gmail.com>
2019-09-10 18:51:30 -0700

    adds actors

--- scraper.rb
+++ scraper.rb
@@ -8,8 +8,11 @@ def set_movie_data(movie_title)
 	open_movie_page(movie_title)
 	runtime
 	title
+	director
+	stars
 end
 
+# Searches movie title and opens movie page in Nokogiri
 def open_movie_page(title)
 	base_url = "https://www.imdb.com/find?ref_=nv_sr_fn&q="
 	title = title.gsub(/\s/, '+')
@@ -18,6 +21,7 @@ def open_movie_page(title)
     @movie = Nokogiri::HTML(open("https://www.imdb.com/#{url}"))
 end
 
+# Returns movie runtime in minutes as an integer
 def runtime
 	runtime = 0
 	time = @movie.css('time')[0].text.strip.delete('min')
@@ -27,19 +31,22 @@ def runtime
 	@runtime = runtime
 end
 
+# Returns movie title
 def title
 	@title = @movie.css('title').text.split('(')[0].strip
 end
 
+# Returns movie year
 def year
 	@year = @movie.css('span#titleYear').text.gsub(/[()]/, '')
 end
 
+# Returns metacritic score as integer
 def metacritic_score
 	@metacritic_score = @movie.css('div.metacriticScore').text.strip.to_i
 end
 
-# Returns an array
+# Returns an array or director or directors
 def director
 	director_glob = @movie.css('div.plot_summary div.credit_summary_item')[0].text.gsub("\n", '')
 	if director_glob.include?('Directors:')
@@ -47,6 +54,12 @@ def director
 	else
 		@director = [director_glob.split('Director').last.strip.gsub(':', '')]
 	end
+end
+
+# Returns an array of headline actors
+def stars
+	@stars = @movie.css('div.plot_summary div.credit_summary_item').last.text.gsub("\n", '')
+		.split('|').first.strip.gsub('Stars:', '').split(',').map{|star| star.strip}
 end
 
 # https://www.imdb.com/find?ref_=nv_sr_fn&q=evil+dead&s=tt
